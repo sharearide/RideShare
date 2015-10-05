0f934005be17317ff5eb98452140286af1cceea5
Arun Agarwal <arun2621993@gmail.com>
2015-10-05 06:16:50 +0530

    new package

---
+++ app/src/main/java/com/example/bunty/sharetheride/Main/FindRideActivity.java
@@ -0,0 +1,17 @@
+package com.example.bunty.sharetheride.Main;
+
+import android.os.Bundle;
+
+/**
+ * Created by bunty on 10/5/2015.
+ */
+public class FindRideActivity extends NavigationDrawer {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+
+    }
+}
