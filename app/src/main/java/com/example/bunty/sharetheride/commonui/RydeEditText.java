912fcc4198ff2b15c9ac58103fad850a6f037241
arun <developer.arunagarwal@gmail.com>
2015-10-09 22:00:52 +0530

    Offer Ride

---
+++ app/src/main/java/com/example/bunty/sharetheride/commonui/RydeEditText.java
@@ -0,0 +1,55 @@
+// Decompiled by Jad v1.5.8e. Copyright 2001 Pavel Kouznetsov.
+// Jad home page: http://www.geocities.com/kpdus/jad.html
+// Decompiler options: braces fieldsfirst space lnc 
+
+package com.example.bunty.sharetheride.commonui;
+
+import android.content.Context;
+import android.graphics.Typeface;
+import android.support.v7.widget.AppCompatEditText;
+import android.util.AttributeSet;
+
+import com.example.bunty.sharetheride.common.FontCache;
+
+public class RydeEditText extends AppCompatEditText
+{
+
+    public RydeEditText(Context context)
+    {
+        super(context);
+        isInEditMode();
+    }
+
+    public RydeEditText(Context context, AttributeSet attributeset)
+    {
+        super(context, attributeset);
+        isInEditMode();
+    }
+
+    public RydeEditText(Context context, AttributeSet attributeset, int i)
+    {
+        super(context, attributeset, i);
+        isInEditMode();
+    }
+
+    public void setCustomTypeFace(Typeface typeface)
+    {
+        super.setTypeface(typeface);
+    }
+
+    public void setTypeface(Typeface typeface)
+    {
+        if (!isInEditMode())
+        {
+            super.setTypeface(FontCache.getTypeface(getContext(), "font/roboto/Roboto-Light.ttf"));
+        }
+    }
+
+    public void setTypeface(Typeface typeface, int i)
+    {
+        if (!isInEditMode())
+        {
+            super.setTypeface(FontCache.getTypeface(getContext(), "font/roboto/Roboto-Light.ttf"));
+        }
+    }
+}
